7760bd025cb3127eff88b0a192eda12223c25b7c
Jesson-hao <105201609+Jesson-hao@users.noreply.github.com>
2023-04-23 20:22:53 +0800

    pull commit

--- src/main/java/com/jesson/GitTest.java
+++ src/main/java/com/jesson/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        SyStem.out.println("pull test");
     }
 }
