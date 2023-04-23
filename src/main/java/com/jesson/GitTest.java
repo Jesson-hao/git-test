aae76e62f3d364558e4cadad9413e1bd3e1bf653
Jesson <Jesson@hao.com>
2023-04-23 17:02:28 +0800

    first commit

---
+++ src/main/java/com/jesson/GitTest.java
@@ -0,0 +1,13 @@
+package com.jesson;
+
+/**
+ * @author Jesson
+ * @create 2023-04-23
+ * @description 学习Git
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
